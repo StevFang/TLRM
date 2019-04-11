9d6e435995f69d83f4341a0c6d458d835454aa55
Brant <fbinemail@163.com>
2019-04-11 12:13:53 +0800

    code modified

--- src/main/java/com/ddmh/utils/DateUtils.java
+++ src/main/java/com/ddmh/utils/DateUtils.java
@@ -1,6 +1,6 @@
 package com.ddmh.utils;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
